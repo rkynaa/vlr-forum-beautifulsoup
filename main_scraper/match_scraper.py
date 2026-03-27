713636cc76a5e67d38f389ab053d0fd01b1bd061
Rakyan Adhikara <rkyn.aa@gmail.com>
2026-03-27 13:15:28 +1100

    Edit Commit (2026/03/27)

--- main_scraper/match_scraper.py
+++ main_scraper/match_scraper.py
@@ -26,6 +26,24 @@ def vct_scraper(vctYearURL):
     vctCompletedEventTitles = [vctLinkEventBlock.find("div", {"class":"event-item-title"}).text.strip() for vctLinkEventBlock in vctLinkCompletedEvents]
     
     vctLinkEventBlocks = vctYearSoup.find_all("a", {"class":"wf-card mod-flex event-item"})
+
+    vctMatchesStats = []
+    colNames = [
+        # Team Name, Aggregated Team Name
+        "team_name", "team_aggr", 
+        # Total map win in a match, 'Best-of' type match (Bo1, Bo3, or Bo5)
+        "total_map_win", "best_of_type",
+        # Pick Team, Match result
+        "pick_team", "match_res",
+        # Map 
+        "map_stats", "total_win", "t_win", "ct_win",
+        "event_name", "match_type",
+        "player_list",
+        "ban_map_1", "ban_map_2",
+        "pick_map_1", "pick_map_2", "pick_map_3",
+        "match_map_1", "match_map_2", "match_map_3", "match_map_4", "match_map_5",
+        "match_url"
+        ]
     
     for vctLinkEventBlock in vctLinkEventBlocks:
         vctEventTitle = vctLinkEventBlock.find("div", {"class":"event-item-title"}).text.strip()
@@ -40,132 +58,136 @@ def vct_scraper(vctYearURL):
             vctLinkEventLst.insert(2, 'matches')
             vctLinkEvent += baseUrl + '/'.join(vctLinkEventLst) + "/?series_id=all"
             
-            match_scraper(baseUrl, vctEventTitle, vctLinkEvent)
-        
-        # print(vctEventTitle, vctLinkEvent)
-            
+            vctMatchesStats.extend(match_scraper(baseUrl, vctEventTitle, vctLinkEvent))
+
+        print(f"Match Stats: {len(vctMatchesStats)}", end='\r', flush=True)
+    
+    print(f"Match Stats: {len(vctMatchesStats)}", end='\r', flush=True)
+    
+    output_file = f"vct_{vctYear}_match_stats.csv"
+
+    with open(output_file, mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+
+        # write header
+        writer.writerow(colNames)
+
+        # write rows
+        writer.writerows(vctMatchesStats)
+
+    print(f"CSV saved as {output_file}")
+
     time.sleep(5)
-    return None
+
+    return vctMatchesStats
+
+    # return None
 
 def match_scraper(baseUrl, vctEventTitle, vctEventMatchLink):
+    # BeautifulSoup for VCT Event's Matches
     vctMatchesSoup = BeautifulSoup(requests.get(vctEventMatchLink).content, 'html.parser')
+
+    # Find all <a> tag for each VCT Matches
     vctLinkMatchBlocks = vctMatchesSoup.find_all("a", {"class":"match-item"})
     print(F"Total match for {vctEventTitle} is {len(vctLinkMatchBlocks)}")
-    for vctLinkMatchBlock in vctLinkMatchBlocks:    
-        vctLinkMatch = baseUrl + vctLinkMatchBlock.get("href")
+
+    # List of VCT matches in this current event
+    vctMatchesLst = []
+
+    # Number of Stats 
+    numStats = 0
+
+    # For each match in list of matches
+    for vctLinkMatchNum in range(len(vctLinkMatchBlocks)):
+
+        # COLUMN: 'match_url'
+        # VCT Match URL (i.e. https://www.vlr.gg/598950/bilibili-gaming-vs-edward-gaming-vct-2026-china-kickoff-lbf)
+        vctLinkMatch = baseUrl + vctLinkMatchBlocks[vctLinkMatchNum].get("href")
+
+        # BeautifulSoup for VCT Match
         vctMatchSoup = BeautifulSoup(requests.get(vctLinkMatch).content, 'html.parser')
 
-        matchTeamsProps = [[], []]
+        matchDivType = vctMatchSoup.find("div", {"class":"match-header-event-series"})
+        matchType = " ".join(matchDivType.text.split())
+
+        if 'showmatch' in matchType.lower():
+            continue
+
+        # print(vctLinkMatch)
+
+        # Getting the bans and picks decisions in the match
+        matchDivMapsDecisions = vctMatchSoup.find("div", {"class":"match-header-note"})
+        matchMapsDecisionsStr = matchDivMapsDecisions.text
+        if ';' not in matchMapsDecisionsStr:
+            continue
         
-        matchScore = "".join(vctMatchSoup.find("div", {"class":"match-header-vs-score"}).text.strip().split()[1:4])
+        matchMapsDecisionsLst = [matchMapsDecs.strip() for matchMapsDecs in matchMapsDecisionsStr.strip().split(';')]
+
+        # VCT Team Stats on the current scraped match
+        matchTeamsProps = [[], []]
+
+        # COLUMN: 'best_of_type'
+        # Getting the 'Best-of' type of match (Bo1, Bo3, Bo5)
         matchBestOfType = vctMatchSoup.find("div", {"class":"match-header-vs-score"}).text.strip().split()[-1]
-        # print(matchType)
+        
+        # Match Score in the current scraped match (in string form)
+        matchScore = "".join(vctMatchSoup.find("div", {"class":"match-header-vs-score"}).text.strip().split()[1:4])
+
+        # COLUMN: 'total_map_win'
+        # Match Score in the current scraped match (in list form)
         matchScoreLst = matchScore.split(':')
         matchScoreIntLst = [int(matchScoreStr) for matchScoreStr in matchScoreLst]
+
+        # Total Match Score (2:1 (or []))
         matchScoreTotal = sum(matchScoreIntLst)
         
+        # COLUMN: 'team_names'
+        # Getting team names
         matchTeamsLinkLst = vctMatchSoup.find_all("a", {"class":"match-header-link"})
         matchTeamNames = [matchTeamsLink.text.strip() for matchTeamsLink in matchTeamsLinkLst]
-        # print(matchTeamNames)
         
+        # CCOLUMN: 'team_aggr'
+        # Getting aggregated team names
         matchStatsAllMaps = vctMatchSoup.find("div", {"class":"vm-stats-game mod-active"})
         matchAllMapsPlayersLst = [" ".join(matchStatsAllMapsPlayer.text.split()[::-1]) for matchStatsAllMapsPlayer in matchStatsAllMaps.find_all("td", {"class":"mod-player"})]
         matchTeamAggrNames = [matchAllMapsPlayersLst[0].split(' ')[0], matchAllMapsPlayersLst[-1].split(' ')[0]]
 
-        matchDivMapsDecisions = vctMatchSoup.find("div", {"class":"match-header-note"})
-        matchMapsDecisionsLst = [matchMapsDecs.strip() for matchMapsDecs in matchDivMapsDecisions.text.strip().split(';')]
-
+        # Getting picked maps (including non-played)
         matchMapsPlaysLst = [matchMapsPlays for matchMapsPlays in matchMapsDecisionsLst if "pick" in matchMapsPlays]
         matchMapsPlaysLst.append(matchMapsDecisionsLst[-1])
+
+        # COLUMN: 'match_map_1', 'match_map_2', 'match_map_3', 'match_map_4', 'match_map_5'
+        # Getting the played maps (picked AND played)
+        print(matchMapsPlaysLst, matchScoreTotal)
         matchMapsPlaysTrueLst = [matchMapsPlaysLst[i] for i in range(matchScoreTotal)]
 
         matchDivGameIDs = vctMatchSoup.find_all("div", {"class":"js-map-switch"})
         matchGameIDs = [matchDivGameID.get("data-game-id") for matchDivGameID in matchDivGameIDs]
         matchGameIDs.remove("all")
         matchGameIDs = matchGameIDs[:matchScoreTotal]
-        # print(matchGameIDs)
+
+        # 
         matchMapUrls = [vctLinkMatch + "/?game=" + matchGameID + "&tab=overview" for matchGameID in matchGameIDs]
 
         matchMapScoreStats = map_scraper(matchMapUrls[1])
 
-        print(matchMapScoreStats)
-
-        # for matchMapUrl in matchMapUrls:
-        #     # print(matchMapUrl)
-        #     map_scraper(matchMapUrl)
-        # # print(matchMapUrls)
+        # print(matchMapScoreStats)
 
         for i in range(matchScoreTotal):
             if 'remains' not in matchMapsPlaysTrueLst[i]:
                 matchMapsPlaysTrueLst[i] = matchMapsPlaysTrueLst[i].split()[-1]
             else:
                 matchMapsPlaysTrueLst[i] = matchMapsPlaysTrueLst[i].split()[0]
-
-        matchDivType = vctMatchSoup.find("div", {"class":"match-header-event-series"})
-        matchType = " ".join(matchDivType.text.split())
-        # print(matchType, matchMapsPlaysTrueLst, matchScoreTotal)
-
-        # for i in range(len(matchTeamsProps)):
-        #     teamName = re.sub(r'\s+',' ',matchTeamNames[i])
-        #     if "(" in teamName:
-        #         matchTeamsProps[i].append(teamName.split('(')[1][:-1])
-        #     else:
-        #         matchTeamsProps[i].append(teamName)
-        #     matchTeamsProps[i].append(matchTeamAggrNames[i])
-
-        #     matchTeamsProps[i].append(matchScoreLst[i])
-
-        #     if matchMapsDecisionsLst[0].split()[0] == matchTeamAggrNames[i]:
-        #         matchTeamsProps[i].append("Team A")
-        #     else:
-        #         matchTeamsProps[i].append("Team B")
-
-        #     if matchScoreIntLst.index(max(matchScoreIntLst)) == i:
-        #         matchTeamsProps[i].append("Match Win")
-        #     else:
-        #         matchTeamsProps[i].append("Match Loss")
-
-        #     matchTeamsProps[i].append(vctEventTitle)
-        #     matchTeamsProps[i].append(matchType)
-
-        #     matchTeamsProps[i].append([playerName for playerName in matchAllMapsPlayersLst if playerName.split()[0] == matchTeamAggrNames[i]])
             
-        #     matchTeamMapsDecs = [matchTeamMaps for matchTeamMaps in matchMapsDecisionsLst if matchTeamMaps.split()[0] == matchTeamAggrNames[i]]
-        #     matchTeamMapsDecs.append(matchMapsDecisionsLst[-1])
-            
-        #     matchTeamsProps[i].append(matchTeamMapsDecs)
-        #     banLst = []
-        #     pickLst = []
-        #     for maps in matchTeamMapsDecs:
-        #         if 'ban' in maps:
-        #             banLst.append(maps.split()[-1])
-        #         elif 'pick' in maps:
-        #             pickLst.append(maps.split()[-1])
-        #         else:
-        #             pickLst.append(maps.split()[0])
+        progress = (numStats / (len(vctLinkMatchBlocks) * 2)) * 100
 
-        #     if len(banLst) != 2:
-        #         for j in range(2-len(banLst)):
-        #             banLst.append('-')
-
-        #     if len(pickLst) != 3:
-        #         for j in range(3-len(pickLst)):
-        #             pickLst.append('-')
-            
-        #     matchTeamsProps[i].append(banLst)
-        #     matchTeamsProps[i].append(pickLst)
-
-        #     matchTeamsProps[i].append(matchMapsPlaysTrueLst)
-
-        #     matchTeamsProps[i].append(vctLinkMatch)
-            
-            # print(matchTeamsProps[i])
         for i in range(len(matchTeamsProps)):
             teamName = re.sub(r'\s+',' ',matchTeamNames[i])
             if "(" in teamName:
                 matchTeamsProps[i].append(teamName.split('(')[1][:-1])
             else:
                 matchTeamsProps[i].append(teamName)
+
             matchTeamsProps[i].append(matchTeamAggrNames[i])
 
             matchTeamsProps[i].append(matchScoreLst[i])
@@ -183,21 +205,6 @@ def match_scraper(baseUrl, vctEventTitle, vctEventMatchLink):
                 matchTeamsProps[i].append("win")
             else:
                 matchTeamsProps[i].append("loss")
-
-            # matchTeamMapStats = []
-            # matchTeamTotalScore = 0
-
-            # for mapStat in matchMapScoreStats:
-            #     mapName = mapStat[0]
-            #     mapScore = mapStat[1][teamSideIdx]
-            #     tSideScore = mapStat[2][teamSideIdx][0]
-            #     ctSideScore = mapStat[2][teamSideIdx][1]
-
-            #     matchTeamMapStats.append([mapName, mapScore, tSideScore, ctSideScore])
-            #     matchTeamTotalScore += mapScore
-
-            # matchTeamsProps[i].append(matchTeamMapStats)
-            # matchTeamsProps[i].append(matchTeamTotalScore)
 
             matchTeamMapStats = []
             matchTeamTotalScore = 0
@@ -229,7 +236,6 @@ def match_scraper(baseUrl, vctEventTitle, vctEventMatchLink):
             matchTeamMapsDecs = [matchTeamMaps for matchTeamMaps in matchMapsDecisionsLst if matchTeamMaps.split()[0] == matchTeamAggrNames[i]]
             matchTeamMapsDecs.append(matchMapsDecisionsLst[-1])
             
-            # matchTeamsProps[i].append(matchTeamMapsDecs)
             banLst = []
             pickLst = []
             for maps in matchTeamMapsDecs:
@@ -241,51 +247,52 @@ def match_scraper(baseUrl, vctEventTitle, vctEventMatchLink):
                     pickLst.append(maps.split()[0])
 
             if len(banLst) != 2:
-                for j in range(2-len(banLst)):
+                for j in range(2 - len(banLst)):
                     banLst.append('-')
 
             if len(pickLst) != 3:
-                for j in range(3-len(pickLst)):
+                for j in range(3 - len(pickLst)):
                     pickLst.append('-')
 
-            if len(matchMapsPlaysTrueLst) != 5:
-                for j in range(5-len(matchMapsPlaysTrueLst)):
-                    matchMapsPlaysTrueLst.append('-')
-            
-            matchTeamsProps[i].append(banLst)
-            matchTeamsProps[i].append(pickLst)
+            matchMapsPlayedFlat = matchMapsPlaysTrueLst.copy()
+            if len(matchMapsPlayedFlat) != 5:
+                for j in range(5 - len(matchMapsPlayedFlat)):
+                    matchMapsPlayedFlat.append('-')
 
-            matchTeamsProps[i].append(matchMapsPlaysTrueLst)
+            matchTeamsProps[i].extend(banLst)
+            matchTeamsProps[i].extend(pickLst)
+            matchTeamsProps[i].extend(matchMapsPlayedFlat)
 
             matchTeamsProps[i].append(vctLinkMatch)
             
-            print(matchTeamsProps[i])
+            vctMatchesLst.append(matchTeamsProps[i])
+            numStats += 1 
 
-        # print(matchAllMapsPlayersLst, matchMapsDecisionsLst)
-        # print(f"{vctEventTitle} ({matchTeamNames[0]} {matchScore} {matchTeamNames[1]}) {vctLinkMatch}")
-        colNames = ["team_name", "team_aggr", "pick_team", "match_res", "map_stats", "total_win", "t_win", "ct_win", "event_name", "match_type", "player_list", "ban_map_1", "ban_map_2",
-                    "pick_map_1", "pick_map_2", "pick_map_3", "match_map_1", "match_map_2", "match_map_3", "match_map_4", "match_map_5", "match_url"]
-        print("\n")
-    return None
+            progress = (numStats / (len(vctLinkMatchBlocks) * 2)) * 100
+            print(f"{vctLinkMatchNum + 1} out of {len(vctLinkMatchBlocks)} scraped. {progress:.1f}%", end='\r', flush=True)
+
+        print(f"{vctLinkMatchNum + 1} out of {len(vctLinkMatchBlocks)} scraped. {progress:.1f}%", end='\r', flush=True)
+
+    print(f"Scraping {vctEventTitle} done! Go to next VCT event...")
+    return vctMatchesLst
 
 def map_scraper(vctMapUrl):
-    # print(vctMapUrl)
+
     mapSoup = BeautifulSoup(requests.get(vctMapUrl).content, 'html.parser')
     
     mapDivStatsHeaders = mapSoup.find_all("div", {"class":"vm-stats-game-header"})
 
     mapStatNames = mapSoup.find_all("div", {"class":"map"})
     mapNames = []
+    
     for mapStatName in mapStatNames:
         mapNames.append(mapStatName.text.strip().split()[0])
-    # print(mapNames)
 
     mapDivScores = mapSoup.find_all("div", {"class":"score"})
     mapScores = []
     for mapDivScore in mapDivScores:
         mapScore = mapDivScore.text.strip()
         mapScores.append(int(mapScore))
-    # mapScores = [mapDivScore.text.strip().split() for mapDivScore in mapDivScores]
 
     mapTSideScores = []
     mapCTSideScores = []
@@ -297,10 +304,8 @@ def map_scraper(vctMapUrl):
     mapSideScores = []
 
     for mapNum in range(len(mapNames)):
-        # print(mapNum, len(mapNames))
         for teamNum in range(2):
             mapSideScores.append([int(mapTSideScores[mapNum][teamNum]), int(mapCTSideScores[mapNum][teamNum])])
-            # print()
 
     mapStatScores = []
 
@@ -312,13 +317,5 @@ def map_scraper(vctMapUrl):
         side_scores = mapSideScores[start:end]
 
         mapStatScores.append([name, scores, side_scores])
-
-
-    # mapSpanTSideScores = mapDivStatsHeader.find_all("span", {"class":"mod-t"})
-    # mapSpanCTSideScores = mapDivStatsHeader.find_all("span", {"class":"mod-ct"})
-    # mapTSideScores = [mapSpanTSideScore.text.strip() for mapSpanTSideScore in mapSpanTSideScores]
-    # mapCTSideScores = [mapSpanCTSideScore.text.strip() for mapSpanCTSideScore in mapSpanCTSideScores]
-
-    # print(mapScores, mapStatScores)
 
     return mapStatScores
